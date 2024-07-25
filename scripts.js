2d59278e4a637d31343579d29498f65df5b1ffa6
SimonLegerLaval <sileg17@ulaval.ca>
2024-07-24 23:59:13 -0400

    Added base content layout

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function ShowNav() {
     document.getElementById("nav").style.display = "inline"; // Show the navbar
     document.getElementById("hamburger").style.display = "none"; // Hide the hamburger
-    document.getElementById("content").style.width = "83.33%"; // 
+    document.getElementById("content").style.width = "83.33%"; // Content gives nav some width
 }
 
 function HideNav() {
     document.getElementById("nav").style.display = "none"; // Hide the navbar
     document.getElementById("hamburger").style.display = "inline"; // Show the hamburger
-    document.getElementById("content").style.width = "100%"; // 
+    document.getElementById("content").style.width = "100%"; // Content absorbs nav's width
 }
